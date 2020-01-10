28ba035ef9000e8b1c563dbd9c4d03bc81df5570
Haew <hi@haew.xyz>
2020-01-10 19:44:49 +0100

    wip

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="#app" class="w-full">
-    <div class="mx-auto max-w-xl p-4" style="height: 400px;">
+    <div class="max-w-xl p-4 mx-auto" style="height: 400px;">
       <toggle v-slot="{ toggled, toggle, off }">
         <div>
-
           <!-- Trigger -->
           <button
-            class="px-4 py-2 rounded bg-gray-700 shadow-lg focus:bg-gray-800"
+            class="px-4 py-2 bg-gray-700 rounded shadow-lg focus:bg-gray-800 focus:outline-none focus:shadow-outline"
             @click="toggle"
           >
-            Toggle this
+            Open modal
           </button>
 
-          <div class="absolute inset-0" style="background: rgba(0,0,0,.5);" v-show="toggled" @click="off" />
+          <div
+            class="absolute inset-0 w-screen h-screen"
+            style="background: rgba(0,0,0,.5);"
+            v-show="toggled"
+            @click="off"
+          />
 
           <!-- Content -->
-          <div v-show="toggled" class="p-4 rounded bg-gray-600 shadow-xl mt-2 relative">
+          <div
+            class="relative flex flex-col p-4 mt-2 bg-gray-600 rounded shadow-xl"
+            v-show="toggled"
+          >
             <div>
-              This is the content. <br/>
+              This is the content. <br />
               It's not necessarily a list, you can add anything there.
             </div>
 
             <button
-              class="mt-3 px-4 py-2 rounded bg-gray-700 shadow-lg focus:bg-gray-800"
+              class="self-end px-4 py-2 mt-3 bg-gray-700 rounded shadow-sm focus:bg-gray-800 hover:shadow-md focus:outline-none focus:shadow-outline"
               @click="toggle"
             >
               Close
             </button>
           </div>
-
         </div>
       </toggle>
     </div>
