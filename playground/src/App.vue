bbb905ac0c5501668126286b46dbde743c04d325
Haew <hi@haew.xyz>
2020-01-10 17:31:15 +0100

    Updated playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <div id="#app">
-    zepfjpzefj
+  <div id="#app" class="w-full">
+    <div class="mx-auto max-w-xl p-4" style="height: 400px;">
+      <dropdown v-slot="{ opened, toggle }">
+
+        <div class="relative">
+          <!-- Trigger -->
+          <button
+            class="px-4 py-2 rounded bg-gray-700 shadow-lg"
+            @click="toggle"
+          >
+            Open dropdown
+          </button>
+
+          <!-- Content -->
+          <div v-show="opened" class="p-4 rounded bg-gray-600 shadow-xl mt-2">
+            This is the dropdown content. <br/>
+            <a href="https://google.fr">Google</a>
+          </div>
+        </div>
+
+      </dropdown>
+    </div>
   </div>
 </template>
 
