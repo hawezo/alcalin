a0ba5c4953d8732184535299df0f4e9c3a69eb76
Haew <hi@haew.xyz>
2020-01-10 12:03:30 +0100

    [WIP] Dropdown

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
-}
+  presets: ['@vue/app'],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,1 @@
-export { default } from './Dropdown.vue';
+export { default } from './Dropdown';
